4116e670c3988558e9690e108d3d733ff2a9c994
Sakha <sakha60@gmail.com>
2020-10-03 22:06:38 +0300

    Events - OK,Staff - isuue with clearing select input

--- src/myFunc.js
+++ src/myFunc.js
@@ -1,21 +1,27 @@
+//  Global variables 'url_dtb'- url of database spreadsheet, 'ss_dtb' - database spreadsheet
+//  shEvent,shStaff - sheets in database spreadsheet
 
+var url_dtb = 'https://docs.google.com/spreadsheets/d/1uvpi8N92RaCmbMFvbsABk3L4xKtkVUBwbRxrsmIY64o/edit#gid=0';
+var ss_dtb = SpreadsheetApp.openByUrl(url_dtb);
+var shEvent = ss_dtb.getSheetByName('Events');
+var shStaff = ss_dtb.getSheetByName('Staff');
+
+//========================================   function write_to_DB   =================================================================
 function write_to_DB(_data, _id) {
-
-  var url = 'https://docs.google.com/spreadsheets/d/1uvpi8N92RaCmbMFvbsABk3L4xKtkVUBwbRxrsmIY64o/edit#gid=0';
-  
-  var ss = SpreadsheetApp.openByUrl(url);
-  var sh = ss.getSheetByName('Events');
   
   var staffRegistered = 0;
   var staffConfirmed = 0;
 
   _data.endDate.setDate(_data.endDate.getDate() - 1);
   
-  sh.appendRow([_id, _data.title, _data.location, _data.startDate, _data.endDate, _data.numberOfStaff, _data.notes, staffRegistered, staffConfirmed]);
-
+  shEvent.appendRow([_id, _data.title, _data.location, _data.startDate, _data.endDate, _data.numberOfStaff, _data.notes, staffRegistered, staffConfirmed]);
+  var color_r = shEvent.getRange('a1').getBackgroundObject().asRgbColor().getRed();
+  var color_g = shEvent.getRange('a1').getBackgroundObject().asRgbColor().getGreen();
+  var color_b = shEvent.getRange('a1').getBackgroundObject().asRgbColor().getBlue();
+  Logger.log('red: ' + color_r + ' green: ' + color_g + ' blue: ' + color_b);
 }
 
-
+//========================================   function createEvent   =================================================================
 function createEvent(_event) {
 
   var calendar = CalendarApp.getCalendarById('9pqgeoe0s2n22jlftlkt2fa73g@group.calendar.google.com');
@@ -35,17 +41,14 @@ _event.endDate.setDate(_event.endDate.getDate() + 1);
   createEventSpreadsheet(eventID);
 }
 
+//========================================   function addPerson   =================================================================
 function addPerson(_pers) {
 
-  var url = 'https://docs.google.com/spreadsheets/d/1uvpi8N92RaCmbMFvbsABk3L4xKtkVUBwbRxrsmIY64o/edit#gid=0';
-  var ss = SpreadsheetApp.openByUrl(url);
-  var sh = ss.getSheetByName('Staff');
-
-  sh.appendRow([_pers.id, _pers.name]);
+  shStaff.appendRow([_pers.id, _pers.name]);
   
 }
 
-
+//========================================   function createStaffSpreadsheet   =================================================================
 function createStaffSpreadsheet(_staff_ID) {
 
   var folder = DriveApp.getFolderById("1-YRer_07CVDFaWRSpB8j3iZTg2pJyOi0");
@@ -54,39 +57,44 @@ function createStaffSpreadsheet(_staff_ID) {
   
   }
   
+//========================================   function createEventSpreadsheet   =================================================================  
 function createEventSpreadsheet(_event_ID) {
 
   folder = DriveApp.getFolderById("16XQff1_JO0T4Vkalb0mPwCz_Uy7Q76Q6");
   var ss = SpreadsheetApp.create(_event_ID);
   DriveApp.getFileById(ss.getId()).moveTo(folder);
+  
+  var event_ss_url = ss.getUrl();
+  shEvent.getRange(shEvent.getLastRow(), 10).setValue(event_ss_url);
+  
   var sh = ss.getSheetByName('Sheet1');
   sh.setName('Registered');
   
   sh.getRange('A1').setValue('staff_ID');
   sh.getRange('B1').setValue('staff_Name');
+  sh.getRange('A1:B1').setBackgroundRGB(255, 229, 153);
+  sh.getRange('A1:B1').setFontWeight("bold");
   
   ss.insertSheet('Confirmed');
   
   sh = ss.getSheetByName('Confirmed');
   sh.getRange('A1').setValue('staff_ID');
   sh.getRange('B1').setValue('staff_Name');
-  
+  sh.getRange('A1:B1').setBackgroundRGB(255, 229, 153);
+  sh.getRange('A1:B1').setFontWeight("bold");
+    
   }
-  
+
+//========================================   function addPersonToEventSheet   =================================================================
 function addPersonToEventSheet(_eventIndex, _staffIndex) {
 
-  _eventIndex = 1;
-  _staffIndex = 1;
-
-  var url = 'https://docs.google.com/spreadsheets/d/1uvpi8N92RaCmbMFvbsABk3L4xKtkVUBwbRxrsmIY64o/edit#gid=0';
-  var ss_dtb = SpreadsheetApp.openByUrl(url);
-  var shEvent = ss_dtb.getSheetByName('Events');
-  var shStaff = ss_dtb.getSheetByName('Staff');
-  
   var urlEvent = shEvent.getRange(_eventIndex + 1, 10).getValue();
   var urlStaff = shStaff.getRange(_staffIndex + 1, 3).getValue();
   var nameStaff = shStaff.getRange(_staffIndex + 1, 2).getValue();
   var idStaff = shStaff.getRange(_staffIndex + 1, 1).getValue();
+  
+
+//      Write pre-registered Staff to Event spreadsheet
 
   var ssEvent = SpreadsheetApp.openByUrl(urlEvent);
   var sh = ssEvent.getSheetByName('Registered');
